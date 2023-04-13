47c590255ef7b8ec3572c81bc2ffce82a1aa791f
Burak <mail4burak@gmail.com>
2023-04-12 23:22:48 -0400

    Docstring added for calculate percantage function

--- app.py
+++ app.py
@@ -1,2 +1,7 @@
 def calculate_percentage(number, percentage):
+    """ Calculates the percentage of the number given.
+                    :param number: Double or int numeric value
+                    :param percentage: Double or int numeric value between 0-100
+                    :return: Calculated percentage
+            """
     return number * percentage / 100
